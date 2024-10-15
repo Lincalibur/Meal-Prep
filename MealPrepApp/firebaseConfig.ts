544223f766ec07e864364186b058f252dfde3a74
Liam Olivier <135514909+Lincalibur@users.noreply.github.com>
2024-10-15 12:37:49 +0200

    Minor changes to hide the API key and sensitive information

--- MealPrepApp/firebaseConfig.ts
+++ MealPrepApp/firebaseConfig.ts
@@ -1,23 +1,26 @@
-// ./MealPrepApp/firebaseConfig.ts
 import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database'; // For Realtime Database (optional: for Firestore, use getFirestore)
-import { getFirestore } from 'firebase/firestore'; // For Firestore if using that
+import { getDatabase } from 'firebase/database';
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_DATABASE_URL,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
+} from '@env';
 
-// Your Firebase configuration details
 const firebaseConfig = {
-  apiKey: "AIzaSyBd_0J6Q_VhnFjRA0UjLvVTWhyZjr10ZY0",
-  authDomain: "mealprepapp-1a0ae.firebaseapp.com",
-  databaseURL: "https://mealprepapp-1a0ae-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "mealprepapp-1a0ae",
-  storageBucket: "mealprepapp-1a0ae.appspot.com",
-  messagingSenderId: "429106749576",
-  appId: "1:429106749576:web:6a6ba5875ef96d55613f3d",
-  measurementId: "G-BZT9HBP2KR"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  databaseURL: FIREBASE_DATABASE_URL,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Realtime Database or Firestore based on your choice
-export const database = getDatabase(app); // For Realtime Database
-export const firestore = getFirestore(app); // For Firestore (if needed)
+export const database = getDatabase(app);
